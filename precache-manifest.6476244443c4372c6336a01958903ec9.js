6b329d6f49df810c7f5fbcd02717b5ae1ba7f613
babykooms <babykooms@users.noreply.github.com>
2021-12-07 15:40:43 -0800

    Updates

---
+++ precache-manifest.6476244443c4372c6336a01958903ec9.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "78cd5a5c8284c68d0cc1",
+    "url": "/abigail-college-portfolio/static/css/main.ad0c7677.chunk.css"
+  },
+  {
+    "revision": "78cd5a5c8284c68d0cc1",
+    "url": "/abigail-college-portfolio/static/js/main.78cd5a5c.chunk.js"
+  },
+  {
+    "revision": "393d64f7851b3067aa72",
+    "url": "/abigail-college-portfolio/static/js/1.393d64f7.chunk.js"
+  },
+  {
+    "revision": "669f933a8002c60e673a",
+    "url": "/abigail-college-portfolio/static/js/runtime~main.669f933a.js"
+  },
+  {
+    "revision": "0565b8c8424df8eb6fc0feed18bf964e",
+    "url": "/abigail-college-portfolio/static/media/pixelPractice.0565b8c8.png"
+  },
+  {
+    "revision": "e0ef49fa071f39949d6ec39894be3ffd",
+    "url": "/abigail-college-portfolio/static/media/apparelshoot1.e0ef49fa.jpg"
+  },
+  {
+    "revision": "da986f2d4b405dce9e66d0935b6dd510",
+    "url": "/abigail-college-portfolio/static/media/caase-banner2.da986f2d.jpg"
+  },
+  {
+    "revision": "29716d72ecc4ec6a241a3297ecf9c34f",
+    "url": "/abigail-college-portfolio/static/media/canary-sunset.29716d72.jpg"
+  },
+  {
+    "revision": "3f3333945aa12d92392abd00aae62f8e",
+    "url": "/abigail-college-portfolio/static/media/camels-2.3f333394.jpeg"
+  },
+  {
+    "revision": "e6ec26159a0960de94e3abde5333ec51",
+    "url": "/abigail-college-portfolio/static/media/essaouira-birds.e6ec2615.png"
+  },
+  {
+    "revision": "b11419355eb5660027ce81c7498bd0e3",
+    "url": "/abigail-college-portfolio/static/media/switz-sunset.b1141935.png"
+  },
+  {
+    "revision": "088a12d522e17f65116b62f722391391",
+    "url": "/abigail-college-portfolio/static/media/abigail2.088a12d5.jpg"
+  },
+  {
+    "revision": "9fc0fd1663495cefa7626d3e4865f536",
+    "url": "/abigail-college-portfolio/static/media/AbigailKumar_November2018.9fc0fd16.pdf"
+  },
+  {
+    "revision": "9163b73cdce3ea8da25f4b9accd4123e",
+    "url": "/abigail-college-portfolio/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.cf6ff7af0a748d372490da25e8e3fc05.js"
+  "/abigail-college-portfolio/precache-manifest.6476244443c4372c6336a01958903ec9.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/abigail-college-portfolio/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
