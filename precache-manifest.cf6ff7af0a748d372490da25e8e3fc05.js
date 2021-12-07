b73d382b987caf07a762d72c16da82e4dfd9bbca
babykooms <babykooms@users.noreply.github.com>
2021-12-07 15:39:24 -0800

    Updates

---
+++ precache-manifest.cf6ff7af0a748d372490da25e8e3fc05.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "31bdfe7395a9ac0cd778",
+    "url": "/static/css/main.f5d0f7fd.chunk.css"
+  },
+  {
+    "revision": "31bdfe7395a9ac0cd778",
+    "url": "/static/js/main.31bdfe73.chunk.js"
+  },
+  {
+    "revision": "393d64f7851b3067aa72",
+    "url": "/static/js/1.393d64f7.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0565b8c8424df8eb6fc0feed18bf964e",
+    "url": "/static/media/pixelPractice.0565b8c8.png"
+  },
+  {
+    "revision": "e0ef49fa071f39949d6ec39894be3ffd",
+    "url": "/static/media/apparelshoot1.e0ef49fa.jpg"
+  },
+  {
+    "revision": "da986f2d4b405dce9e66d0935b6dd510",
+    "url": "/static/media/caase-banner2.da986f2d.jpg"
+  },
+  {
+    "revision": "29716d72ecc4ec6a241a3297ecf9c34f",
+    "url": "/static/media/canary-sunset.29716d72.jpg"
+  },
+  {
+    "revision": "3f3333945aa12d92392abd00aae62f8e",
+    "url": "/static/media/camels-2.3f333394.jpeg"
+  },
+  {
+    "revision": "e6ec26159a0960de94e3abde5333ec51",
+    "url": "/static/media/essaouira-birds.e6ec2615.png"
+  },
+  {
+    "revision": "b11419355eb5660027ce81c7498bd0e3",
+    "url": "/static/media/switz-sunset.b1141935.png"
+  },
+  {
+    "revision": "088a12d522e17f65116b62f722391391",
+    "url": "/static/media/abigail2.088a12d5.jpg"
+  },
+  {
+    "revision": "9fc0fd1663495cefa7626d3e4865f536",
+    "url": "/static/media/AbigailKumar_November2018.9fc0fd16.pdf"
+  },
+  {
+    "revision": "6a637c3605d3be4d510f6e5b2490c0ae",
+    "url": "/index.html"
+  }
+];
